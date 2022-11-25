cebbe7e7a46cab7340e5b32d900ccf0f4cd3efed
faneleedison-ux <103309019+faneleedison-ux@users.noreply.github.com>
2022-11-25 17:52:43 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
-import SignUp from './components/Login/SignUp';
 
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
-import Artwork from './components/Artwork/Artwork';
-import About from './components/About/About';
+
 import Creators from './components/Creators/Creators';
 
 
@@ -24,20 +13,7 @@ import Creators from './components/Creators/Creators';
    
   return (
     
-    <Router>
-            
-           <Routes>
-                 <Route exact path='/' element={< Home />}></Route>
-                 <Route exact path='/artwork' element={< Artwork />}></Route>
-                
-                 <Route exact path='/about' element={< About />}></Route>
-                 <Route exact path='/creators' element={< Creators />}></Route>
-                 <Route exact path='/contact' element={< Contact />}></Route>
-               
-                 <Route exact path='/sign-up' element={< SignUp />}></Route>
-          </Routes>
-         
-       </Router>
+    <Creators/>
   );
 }
 
